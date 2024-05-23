f1c8ef12cc7520d723e16c161c4a25a18a5f9582
Gourav Halder <halder.gourav@gmail.com>
2024-05-23 23:02:35 +0200

    23/05

--- general/dictionaries.py
+++ general/dictionaries.py
@@ -2,12 +2,10 @@
 with the requirement that the keys are unique (within one dictionary). 
 A pair of braces creates an empty dictionary: {}."""
 
-tel = {'jack': 4098, 'sape': 4139}
-tel['guido'] = 4127
-print (tel)
-print (tel['jack'])
-del (tel['sape'])
-print (tel)
-print (list(tel))
-print (sorted(tel)) # sorts as per the Dict Keys
-print ('guidoo' in tel)
+tel = ({'jack': 4098, 'sape': 4139},{'bombie': 1111, 'bulai': 2222},{'bombie1': 3333, 'bulai2': 4444})
+for x in tel:
+    for name, number in x.items():
+        print (f"{name} and {number}")
+Ltel=list(tel)
+Ltel.append(1)
+print(Ltel)
